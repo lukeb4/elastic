7c39602ee92863a8dea3fcef8261f99811a0d60b
lukeb4 <37591861+lukeb4@users.noreply.github.com>
2019-01-14 14:00:37 +0100

    asdasd
    asdas

--- script-update.sh
+++ script-update.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 echo "Updating packages via update-script.sh ..."
-sudo apt-get update -y
-sudo apt install python-pip
-pip install --upgrade pip
-pip install docker-compose
 vi test123.txt
